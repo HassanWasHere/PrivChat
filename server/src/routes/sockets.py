250655ac645af54e67d38e952703e823543c1342
Hassan <galacticalliancerblx@gmail.com>
2021-12-08 18:32:55 +0000

    more classes!

--- server/src/routes/sockets.py
+++ server/src/routes/sockets.py
@@ -4,43 +4,46 @@ from flask import request
 from flask_socketio import SocketIO, emit, join_room
 import json
 
-authenticated_sockets = dict()
+class WebSocketServer:
+    def __init__(self, socketio):
+        self.__authenticated_sockets = dict()
+        self.__socketio = socketio
 
-def get_user_from_sid(s_id):
-    for username in authenticated_sockets:
-        if s_id == authenticated_sockets[username]:
-            return users.User.create_from_username(username)
+    def get_user_from_sid(self, s_id):
+        for username in self.__authenticated_sockets:
+            if s_id == self.__authenticated_sockets[username]:
+                return users.User.create_from_username(username)
 
-def create_socket_routes(socketio):
-    @socketio.on("auth")
-    def auth(username, password):
-        user = users.User.create_from_username(username)
-        if password_hash.verify_user(user, password):
-            authenticated_sockets[username] = request.sid
-            socketio.emit("conversation", json.dumps(user.get_conversations()), to=request.sid)
-                
-    @socketio.on('conversations')
-    def return_conversations():
-        user = get_user_from_sid(request.sid)
-        if user:
-            socketio.emit("conversation", json.dumps(user.get_conversations()), to=request.sid)
+    def create_socket_routes(self):
+        @self.__socketio.on("auth")
+        def auth(username, password):
+            user = users.User.create_from_username(username)
+            if password_hash.verify_user(user, password):
+                self.__authenticated_sockets[username] = request.sid
+                self.__socketio.emit("conversation", json.dumps(user.get_conversations()), to=request.sid)
+                    
+        @self.__socketio.on('conversations')
+        def return_conversations(self):
+            user = self.get_user_from_sid(request.sid)
+            if user:
+                self.__socketio.emit("conversation", json.dumps(user.get_conversations()), to=request.sid)
 
-    @socketio.on("message")
-    def send_message(recipient_id, content):
-        print(f"RECIEVED MESSAGE {recipient_id} {content}")
-        user = get_user_from_sid(request.sid)
-        if user:
-            recipient = users.User.create_from_id(recipient_id)
-            if recipient:
-                #try:
-                message_id = user.send_message(recipient.get_user_id(), content)
-                recipient_room = None
-                for username in authenticated_sockets:
-                    if username == recipient.get_username():
-                        recipient_room = authenticated_sockets[username]
-                if recipient_room:
-                    socketio.emit("message", [user.get_user_id(), content], to=recipient_room)
-                print("MESSAGE ID IS " + str(message_id))
-                return message_id
-                #except:
-                #    print("no")
+        @self.__socketio.on("message")
+        def send_message(self, recipient_id, content):
+            print(f"RECIEVED MESSAGE {recipient_id} {content}")
+            user = self.get_user_from_sid(request.sid)
+            if user:
+                recipient = users.User.create_from_id(recipient_id)
+                if recipient:
+                    #try:
+                    message_id = user.send_message(recipient.get_user_id(), content)
+                    recipient_room = None
+                    for username in self.__authenticated_sockets:
+                        if username == recipient.get_username():
+                            recipient_room = self.__authenticated_sockets[username]
+                    if recipient_room:
+                        self.__socketio.emit("message", [user.get_user_id(), content], to=recipient_room)
+                    print("MESSAGE ID IS " + str(message_id))
+                    return message_id
+                    #except:
+                    #    print("no")
--- server/src/routes/start.py
+++ server/src/routes/start.py
@@ -1,17 +1,24 @@
 import flask
-from flask_socketio import SocketIO, emit
+from flask_socketio import SocketIO
 from src.routes import signup, messages, user, sockets
 
-app = flask.Flask(__name__)
-app.debug = True
+class Server(flask.Flask):
+    def __init__(self):
+        super().__init__(__name__)
+        self.debug = True
+        self.__socketio = None
+    def setup_routes(self):
+        signup.create_route(self)
+        messages.create_route(self)
+        user.create_route(self)
+    def setup_websockets(self):
+        self.__socketio = SocketIO(self, cors_allowed_origins="*")
+        wss_server = sockets.WebSocketServer(self.__socketio)
+        wss_server.create_socket_routes()
+    def start(self):
+        self.__socketio.run(app, host="localhost", port=8080)
 
-signup.create_route(app)
-messages.create_route(app)
-user.create_route(app)
-
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-sockets.create_socket_routes(socketio)
-
-def start_listening():
-    socketio.run(app, host="localhost", port=8080)
+app = Server()
+app.setup_routes()
+app.setup_websockets()
+app.start()
